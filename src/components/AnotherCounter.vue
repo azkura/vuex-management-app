9ded117d0f19bf7fd87e7905250d239584b6911a
az <azizkoura@hotmail.com>
2019-05-31 06:01:09 +0200

    Using Mutations(setters)

---
+++ src/components/AnotherCounter.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <button class="btn btn-primary" @click="increment">Increment</button>
+        <br><br>
+        <button class="btn btn-primary" @click="decrement">Decrement</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            increment() {
+                this.$store.commit('increment')
+            },
+            decrement() {
+                this.$store.commit('decrement')
+            }
+        }
+    }
+</script>
